c1e77a0dfc70488e97b259d50bcbbf6914bd569c
Shazzad Hosen Zisan <mdshazzadhosenzisan@gmail.com>
2026-02-13 17:42:48 +0600

    configure database

---
+++ src/config/env.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config({ quiet: true });
+
+export const ENV = {
+  PORT: process.env.PORT,
+  MONGO_DB_URL: process.env.MONGO_DB_URL,
+  NODE_ENV: process.env.NODE_ENV,
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY,
+  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+  REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY,
+};
