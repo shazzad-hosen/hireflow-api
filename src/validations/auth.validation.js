21fba779aad00965a98fdabf7ee53189536eec41
Shazzad Hosen Zisan <mdshazzadhosenzisan@gmail.com>
2026-02-14 18:46:09 +0600

    add joi validation for regiser and login

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -1,1 +1,38 @@
+import Joi from "joi";
 
+// Schema Validation For Register
+export const registerSchema = Joi.object({
+  name: Joi.string().trim().min(3).max(25).required().messages({
+    "string.empty": "Name is required",
+    "string.min": "Name must be at least 3 characters",
+    "string.max": "Name cannot exceed 25 characters",
+  }),
+
+  email: Joi.string().trim().email().required().messages({
+    "string.email": "Invalid email format",
+    "string.empty": "Email is required",
+  }),
+
+  password: Joi.string().min(6).max(30).required().messages({
+    "string.min": "Password must be at least 6 characters",
+    "string.empty": "Password is required",
+  }),
+}).options({
+  abortEarly: false,
+  allowUnknown: false,
+});
+
+// Schema Validation For Login
+export const loginSchema = Joi.object({
+  email: Joi.string().trim().email().required().messages({
+    "string.email": "Invalid email format",
+    "string.empty": "Email is required",
+  }),
+
+  password: Joi.string().required().messages({
+    "string.empty": "Password is required",
+  }),
+}).options({
+  abortEarly: false,
+  allowUnknown: false,
+});
